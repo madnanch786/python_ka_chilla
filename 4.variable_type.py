48e4043d0c01254dfa8de9e01681d63fe3fc0bb3
madnanch786 <madnanch2016@gmail.com>
2025-11-23 05:17:13 -0800

    Added: Operator.py

---
+++ 4.variable_type.py
@@ -0,0 +1,22 @@
+# There are Four Basic type of Variables
+#1. Integer denoted by int (a=5 whole no without fractions)
+#2. Floating pint/fractional denoted by float (a=969.63 with fraction)
+#3. String denoted by str (Stores non numeric values a="It is a string")
+#4. Boolean denoted by bool (It Stores/returns True or False)
+
+print("Different Type of variables")
+a=67 # integer type
+var_type=type(a)
+print(f"The type of Variable containing Value {a} is {var_type}")
+b=67.566 # float type
+var_type=type(b)
+print(f"The type of Variable containing Value {b} is {var_type}")
+c="Adnan Saleem" # String type
+var_type=type(c)
+print(f"The type of Variable containing Value {c} is {var_type}")
+d=True # Boolean type
+var_type=type(d)
+print(f"The type of Variable containing Value {d} is {var_type}")
+e=int(d)
+var_type=type(e)
+print(f"The type of Variable containing Value {int(e)} is {var_type}")
