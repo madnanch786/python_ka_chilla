21c0bf7c08f93eed1c83b2423c77541d2716ef43
madnanch786 <madnanch2016@gmail.com>
2025-11-22 21:06:11 -0800

    Added:print_hello_word

---
+++ 1.print_hello_word.py
@@ -0,0 +1,22 @@
+# Here we will learn about to print
+
+#Print Hello! World
+print("Printing of String Using print statements with various quotations:")
+print("Hello! World") # Using Double ""
+print('Hello! World') # Using Single ''
+print('''Hello! World''') #using triple '''
+
+#print Hello! World Using variable
+print("Printing of String Using Variables with various quotations:")
+name='Hello! World'
+name1="Hello! World"
+name2='''Hello! World'''
+print(name,"\t",name1,"\t",name2)
+
+#Print statements without loop multiple times by using * operator
+print("M ADNAN SALEEM"*4)
+
+#print the variables by using the format string
+name="Muhammad Adnan Saleem"
+age=35
+print(f"My Name is {name}\nI am {age} years old")
