71c35b58d4b309c10bce08b8ddd2e665add923bd
madnanch786 <madnanch2016@gmail.com>
2025-11-22 21:43:10 -0800

    ADDED:Comment.py

---
+++ 3.Comments.py
@@ -0,0 +1,8 @@
+# Comments are the non executable statements which are written to explain the code it can single line and multiline
+
+# Single line Comment is started with # and have no ending symbol
+'''
+Multiline Comments:
+started with  triple quotation
+and ends with triple quotation
+'''
